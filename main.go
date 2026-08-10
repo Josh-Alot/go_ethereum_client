3dfa486209d81063aa07ae95cf39732e01a10daf
Josh <josh_alot@protonmail.com>
2026-08-10 20:38:50 -0300

    creating a single function to get new client connections to RPC
    creating a function to be called only when an RPC connection must be
    made

--- main.go
+++ main.go
@@ -10,22 +10,20 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: blocknumber, getbalance, blockinfo, txinfo")
+		fmt.Println("usage: blocknumber, getbalance, blockinfo, txinfo, createaccount, listaccounts")
 		os.Exit(1)
 	}
-
-	client, err := NewClient(rpcURL)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
-
-	defer client.Close()
 
 	switch os.Args[1] {
 	case "blocknumber":
 		blocknumber := flag.NewFlagSet("blocknumber", flag.ExitOnError)
 		blocknumber.Parse(os.Args[2:])
+
+		client, err := createClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
 
 		height, err := client.BlockHeight()
 		if err != nil {
@@ -45,6 +43,12 @@ func main() {
 			os.Exit(1)
 		}
 
+		client, err := createClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
+
 		balance, err := client.Balance(*address)
 		if err != nil {
 			log.Fatal(err)
@@ -62,6 +66,12 @@ func main() {
 			fmt.Printf("must give a block height\n")
 			os.Exit(1)
 		}
+
+		client, err := createClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
 
 		summary, err := client.BlockInfo(big.NewInt(int64(*height)))
 		if err != nil {
@@ -81,6 +91,12 @@ func main() {
 			os.Exit(1)
 		}
 
+		client, err := createClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
+
 		tx, err := client.TransactionByHash(*txHash)
 		if err != nil {
 			log.Fatal(err)
@@ -88,8 +104,42 @@ func main() {
 
 		FormatTxSummary(*tx)
 
+	case "createaccount":
+		createaccount := flag.NewFlagSet("createaccount", flag.ExitOnError)
+		password := createaccount.String("password", "", "the encryption password")
+
+		createaccount.Parse(os.Args[2:])
+
+		if *password == "" {
+			fmt.Printf("provide the encryption password\n")
+			os.Exit(1)
+		}
+
+		account, err := CreateAccount(*password)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		FormatAccountCreated(*account)
+
+	case "listaccounts":
+		listaccounts := flag.NewFlagSet("listaccounts", flag.ExitOnError)
+		listaccounts.Parse(os.Args[2:])
+
+		accounts := ListAccounts()
+		FormatAccountsList(accounts)
+
 	default:
 		fmt.Printf("command not found\n")
 		os.Exit(1)
 	}
+}
+
+func createClient() (*Client, error) {
+	client, err := NewClient(rpcURL)
+	if err != nil {
+		return nil, fmt.Errorf("client connection error: %w", err)
+	}
+
+	return client, nil
 }
