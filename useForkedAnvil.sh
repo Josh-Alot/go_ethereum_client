488c570bdc76102c736f2d356d70b576eb15eb0a
Josh <josh_alot@protonmail.com>
2026-09-05 16:25:31 -0300

    update useForkedAnvil.sh
    updating the script so it can use a pre-configured RPC like Alchemy,
    Infura, etc

--- useForkedAnvil.sh
+++ useForkedAnvil.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source ./.secrets.sh
 mkdir -p .anvil/
-anvil --fork-url "https://ethereum-sepolia-rpc.publicnode.com"
+anvil --fork-url "${SEPOLIA_RPC_URL?:define SEPOLIA_RPC_URL on .secrets.sh}"
